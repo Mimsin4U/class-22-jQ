0313d8d57dd0ca07daadb7c95277d71e989bf54b
Mimsin4U <mimsinjd@gmail.com>
2019-07-09 18:33:17 +0600

    add

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$('#btn').click(function () {
+    var FName = $('#firstName').val();
+    var LName = $('#lastName').val();
+    var Sum = FName+' '+LName;
+    $('#fullName').val(Sum);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
